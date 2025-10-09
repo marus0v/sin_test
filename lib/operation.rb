536a5cfe71d6536b88b452f87da14370f2c9bb40
Andrey <andrey.marus@gmail.com>
2025-10-09 05:53:38 +0300

    Operation to DB

--- lib/operation.rb
+++ lib/operation.rb
@@ -13,9 +13,17 @@ class Operation < ActiveRecord::Base
   #  return result.to_i + 1
   # end
 
+  def self.get_last_id
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+    result = db.execute("SELECT MAX(id) FROM operations").flatten.first
+    db.close
+    return result || 0
+  end
+
   def self.count(info)
+    new_operation_id = (Operation.get_last_id + 1).to_i
     user_inf =  User.get_user_by_id(info['user_id']).flatten #.to_s
-    user_template_id = User.get_template_by_id(user_inf[1]) #.flatten.to_sruby
+    user_template_id = User.get_template_by_id(user_inf[1]) #.flatten.to_s
     user_discount = Level.get_discount_by_id(user_template_id[0]).to_i
     # discount = 0
     # discount_level = Level.get_discount_by_id(user_template_id[0]).to_i
@@ -38,8 +46,8 @@ class Operation < ActiveRecord::Base
     total_items = info['positions'].sum { |p| p['quantity'] }
     total_price = info['positions'].sum { |p| p['price'] * p['quantity']}
     total_discounted_price = discounted_positions.sum { |p| p['price'] * p['quantity']}
+    puts total_discounted_price
     discount_summ = (discounted_positions.sum { |p| p['discount_summ'].to_i }).to_f.round(1)
-    # order_summ = info['positions'].sum { |p| p['price'] * p['quantity']}
     discount_value = ((discount_summ.to_f / total_price ) * 100).round(2)
     discount = {
         'summ' => discount_summ,
@@ -55,16 +63,8 @@ class Operation < ActiveRecord::Base
         'price' => discounted_price,
         'quantity' => p['quantity'],
         'type' => product_inf['type'],
-        # 'type' => Product.get_product_discount_cashback(p['id'])['type'], #.to_s,
-        # 'value' => Product.get_value_by_id(p['id']),
-        # 'cashback_value' => Product.get_product_discount_cashback(p['id'])['cashback_value'],
         'cashback_value' => product_inf['cashback_value'],
-        # 'type_desc' => Product.get_type_desc(p['id']),
         'bonus_max_writeoff' => p['type'].to_s == 'noloyalty' ? 0 : (discounted_price * p['quantity'].to_i),
-        # case p['type']
-        # when 'noloyalty'
-        #   'bonus_max_writeoff' => 0
-        # when   : (p['price'] * p['quantity)']),
         'bonus_add' => p['type'].to_s == 'noloyalty' ? 0 : ((discounted_price * (user_cashback + product_inf['cashback_value']) * p['quantity'] / 100).to_i),
         # 'discount_percent' => Product.get_discount_percent(p['id']),
         'discount_summ' => p['price'] - p['price'] * (100 - Product.get_discount_percent(p['id']).to_i) / 100
@@ -72,7 +72,9 @@ class Operation < ActiveRecord::Base
     end
     max_writeoff = (cashback_positions.sum { |p| p['bonus_max_writeoff'].to_i }).to_f.round(1)
     total_bonus_add = (cashback_positions.sum { |p| p['bonus_add'].to_i }).to_f.round(0)
+    puts total_discounted_price
     total_bonus_percent = ((total_bonus_add / total_discounted_price.to_f) * 100).to_f .round(2)
+    puts total_discounted_price
     cashback = {
         'existed_summ': user_inf[3],
         # 'allowed_summ': 434.0,
@@ -87,90 +89,117 @@ class Operation < ActiveRecord::Base
     # end
 
     # return
-    {
-      # user_id: info['user_id'],
+    result = {
       status: 200,
-      discount_level: user_discount,# discount_level,
-      # user_info: user_inf,
       user: {
         id: user_inf[0],
         template_id: user_inf[1],
         name: user_inf[2],
         bonus: user_inf[3]
       },
-      operation_id: 666,
-      summ: 734.0,
+      operation_id_01: 666,
+      operation_id: new_operation_id,
+      summ: total_discounted_price.to_f.round(1),
+      summ_01: 734.0,
       positions: discounted_positions,
-      # user_template_id: user_template_id, #[0].to_i,
-      # template: level_id['id'].to_i,
       discount: discount,
-      cashback: cashback,
-      cashback_positions: cashback_positions
-      #positions_count: positions_count,
+      cashback: cashback #,
+      # cashback_positions: cashback_positions
+      # positions_count: positions_count,
       # positions: positions,
       #discounted_positions: discounted_positions,
       #total_items: total_items,
       #total_price: total_price,
       #total_discounted_price: total_discounted_price
     }
+    # result.save_to_db
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+    db.results_as_hash = true
+    # db.execute(
+    #      "INSERT INTO operations ("
+    #      id
+    #      ")
+    #      VALUES ("
+    #      result['operation_id']
+    #      ")"# ,
+    #    # result.values # массив значений хэша, которые будут вставлены в запрос вместо _плейсхолдеров_
+    #  )
+    db.execute("INSERT INTO operations (id, user_id, cashback, cashback_percent, discount, discount_percent, write_off, allowed_write_off, check_summ, done) " +
+    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [result[:operation_id], result[:user][:id], result[:cashback][:will_add], result[:cashback][:value], result[:discount]['summ'], result[:discount]['value'], result[:cashback][:allowed_summ], result[:cashback][:allowed_summ], result[:summ], 'false'])
+    db.close
+    return result
   end
 
-  def self.get_products
+  # def self.get_products
         # db = SQLite3::Database.open(@@SQLITE_DB_FILE) # открываем "соединение" к базе SQLite
         # db.results_as_hash = true # настройка соединения к базе, он результаты из базы преобразует в Руби хэши
         # db.execute("SELECT * FROM Users")
         # закрываем соединение
         #db.close
 
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
-    result = db.execute("SELECT * FROM Products")
-    db.close
+  #  db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+  #  result = db.execute("SELECT * FROM Products")
+  #  db.close
     # result.to_s
-    return result
-  end
+  #  return result
+  # end
 
-  def self.get_product_by_id(id)
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
-    result = db.execute("SELECT * FROM Products WHERE id = ?", id.to_i)
-    db.close
-    return result
-  end
+  # def self.get_product_by_id(id)
+  #  db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+  #  result = db.execute("SELECT * FROM Products WHERE id = ?", id.to_i)
+  #  db.close
+  #  return result
+  # end
 
-  def self.get_name_by_id(id)
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
-    result = db.execute("SELECT name FROM Products WHERE id = ?", id.to_i)
-    db.close
-    return result
-  end
+  # def self.get_name_by_id(id)
+  #  db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+  #  result = db.execute("SELECT name FROM Products WHERE id = ?", id.to_i)
+  #  db.close
+  #  return result
+  # end
 
-  def self.get_rule_by_id(id)
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
-    result = db.execute("SELECT type, value FROM Products WHERE id = ?", id.to_i).flatten
-    db.close
-    return result
-  end
+  # def self.get_rule_by_id(id)
+  #  db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+  #  result = db.execute("SELECT type, value FROM Products WHERE id = ?", id.to_i).flatten
+  #  db.close
+  #  return result
+  # end
     
-  def save_to_db
+  def self.save_to_db
         db = SQLite3::Database.open(@@SQLITE_DB_FILE) # открываем "соединение" к базе SQLite
         db.results_as_hash = true # настройка соединения к базе, он результаты из базы преобразует в Руби хэши
     
         # запрос к базе на вставку новой записи в соответствии с хэшом, сформированным дочерним классом to_db_hash
         db.execute(
-          "INSERT INTO posts (" +
-            to_db_hash.keys.join(', ') + # все поля, перечисленные через запятую
+          "INSERT INTO operations (" +
+            result.keys.join(', ') + # все поля, перечисленные через запятую
             ") " +
             " VALUES ( " +
-            ('?,'*to_db_hash.keys.size).chomp(',') + # строка из заданного числа _плейсхолдеров_ ?,?,?...
+            ('?,'*result.keys.size).chomp(',') + # строка из заданного числа _плейсхолдеров_ ?,?,?...
             ")",
-          to_db_hash.values # массив значений хэша, которые будут вставлены в запрос вместо _плейсхолдеров_
+          result.values # массив значений хэша, которые будут вставлены в запрос вместо _плейсхолдеров_
         )
     
-        insert_row_id = db.last_insert_row_id
+        # insert_row_id = db.last_insert_row_id
     
         # закрываем соединение
         db.close
     
         # возвращаем идентификатор записи в базе
         return insert_row_id
+  end
+
+  def self.get_users
+    # db = SQLite3::Database.open(@@SQLITE_DB_FILE) # открываем "соединение" к базе SQLite
+    # db.results_as_hash = true # настройка соединения к базе, он результаты из базы преобразует в Руби хэши
+    # db.execute("SELECT * FROM Users")
+    # закрываем соединение
+    #db.close
+
+    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
+    result = db.execute("SELECT * FROM Users")
+    db.close
+    # result.to_s
+    return result
   end
 end
